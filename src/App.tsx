11bb867986d75648b1e9c2106baea852c2c660ad
Marianna_Po <mapodmasko@gmail.com>
2023-06-28 14:36:25 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         <div><img src='https://static.mk.ru/upload/entities/2022/05/21/08/articles/detailPicture/40/81/35/64/5c87906a425717ea86b2041a8aaeb273.jpg'/> </div>
         <div>post</div>
         <div>new post</div>
-        <div>post 1 </div>
+        <div>post 1 </div>дад
         <div>post 2</div>
         </div>
     </div>
