c452f0b48fe3dd571b7291fe190961ed09da9ba9
Marianna_Po <mapodmasko@gmail.com>
2023-06-28 14:34:06 +0300

    after 11 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Month from './components/Month';
+
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Month/>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://motor.ru/thumb/1500x0/filters:quality(75):no_upscale()/imgs/2021/09/16/13/4897225/feef38d97c46d9cab3d016e622ab0c8773d6e7a4.jpg'/>
+      </header>
+      <nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <div><img src='https://s0.rbk.ru/v6_top_pics/media/img/8/17/346838786205178.jpg'/></div>
+        <div><img src='https://static.mk.ru/upload/entities/2022/05/21/08/articles/detailPicture/40/81/35/64/5c87906a425717ea86b2041a8aaeb273.jpg'/> </div>
+        <div>post</div>
+        <div>new post</div>
+        <div>post 1 </div>
+        <div>post 2</div>
+        </div>
     </div>
   );
 }
