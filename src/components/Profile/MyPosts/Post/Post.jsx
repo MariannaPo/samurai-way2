107d33b8923d672c931009f5fbc8116984ee441d
Marianna_Po <mapodmasko@gmail.com>
2023-07-27 13:03:38 +0300

    after 15 lesson correct

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './Post.module.css'
+
+export const Post =()=>{
+    return(
+        <div>
+         
+        <div className={s.items}>
+        <img src='https://static.mk.ru/upload/entities/2022/05/21/08/articles/detailPicture/40/81/35/64/5c87906a425717ea86b2041a8aaeb273.jpg'/>
+            post 1 </div>
+            <span>LIKE</span>
+        </div>
+    )
+}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import s from './Profile.module.css'
+import s from './Profile.module.css';
+import { MyPosts } from "./MyPosts/MyPosts";
+
 
 export const Profile =()=>{
     return(
         <div className={s.content}>
         <div><img src='https://s0.rbk.ru/v6_top_pics/media/img/8/17/346838786205178.jpg'/></div>
-        <div><img src='https://static.mk.ru/upload/entities/2022/05/21/08/articles/detailPicture/40/81/35/64/5c87906a425717ea86b2041a8aaeb273.jpg'/> </div>
-        <div>post</div>
-        <div>new post</div>
-        <div className={s.items}>post 1 </div>
-        <div className={s.items}>post 2</div>
+         <MyPosts/>
         </div>
     )
 }
