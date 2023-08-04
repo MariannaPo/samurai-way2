9829c146635e4ae4d8765febdcc8e2a2883b6e12
Marianna_Po <mapodmasko@gmail.com>
2023-08-04 15:23:50 +0300

    after  18 lessons correct

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import { MyPosts } from "./MyPosts/MyPosts";
 
 export const Profile =()=>{
     return(
-        <div className={s.content}>
+        <div>
         <div><img src='https://s0.rbk.ru/v6_top_pics/media/img/8/17/346838786205178.jpg'/></div>
          <MyPosts/>
         </div>
