90c89950b99419f4a76bf35202efe197876b8a6b
Marianna_Po <mapodmasko@gmail.com>
2023-11-27 19:58:37 +0300

    correct 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import { Header } from './components/Header/Header';
 import { Nav } from './components/Nav/Nav';
 import { Profile } from './components/Profile/Profile';
 import { Dialogs } from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
      <Header/>
      <Nav/>
      <div className='app-wrapper-content'>
-       {/* <Dialogs/> */}
-       <Profile/>
+      <Route path='/messages' component={Dialogs}/>
+      <Route path='/profile' component={Profile}/>
        </div>
-    
-     {/* <Profile/> */}
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,8 +4,8 @@ import s from'./Nav.module.css'
 export const Nav = ()=> {
     return(
         <nav className={s.nav}>
-        <div className={`${s.items} ${s.active}`}><a>Profile</a></div>
-        <div className={s.items}><a>Messages</a></div>
+        <div className={`${s.items} ${s.active}`}><a href="/profile">Profile</a></div>
+        <div className={s.items}><a href="/messages">Messages</a></div>
         <div className={s.items}><a>News</a></div>
         <div className={s.items}><a>Music</a></div>
         <div className={s.items}><a>Settings</a></div>
