800a64769a520f3c5becb4e50cfc17d76010b683
Marianna_Po <mapodmasko@gmail.com>
2023-11-27 20:07:35 +0300

    correct after 19

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,9 +7,9 @@ export const Nav = ()=> {
         <nav className={s.nav}>
         <div className={`${s.items} ${s.active}`}><NavLink to="/profile">Profile</NavLink></div>
         <div className={s.items}><NavLink to="/messages">Messages</NavLink></div>
-        <div className={s.items}><a href="/news">Newsn</a></div>
-        <div className={s.items}><a href="/music">Music</a></div>
-        <div className={s.items}><a href="/settings">Settings</a></div>
+        <div className={s.items}><NavLink to="/news">News</NavLink></div>
+        <div className={s.items}><NavLink to="/music">Music</NavLink></div>
+        <div className={s.items}><NavLink to="/settings">Settings</NavLink></div>
       </nav>
     )
 }
