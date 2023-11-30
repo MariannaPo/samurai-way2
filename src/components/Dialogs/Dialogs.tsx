a181fe11362e9336e76cc4bb55fd837a43fde291
Marianna_Po <mapodmasko@gmail.com>
2023-11-30 15:42:04 +0300

    correct after 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return(
-        <div>
-                <h1>DIALOGS</h1>
+        <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>Vladimir</div>
+                    <div className={s.dialog}>Ilon</div>
+                    <div className={s.dialog}>Sergey</div>
+                    <div className={s.dialog}>Elvira</div>
+                </div>
+            <div className={s.messages}>
+                <div className={s.message}>Будет новый закон</div>
+                <div className={s.message}>Я уже сделал новую машину</div>
+                <div className={s.message}>Открываю новую станцию, спишемся позже</div>
+                <div className={s.message}>Курс не упадет</div>
+            </div>
         </div>
     )
 }
