043b051bdd1103241fd149e0fc4c70902635d2c9
Marianna_Po <mapodmasko@gmail.com>
2023-11-30 16:05:11 +0300

    correct after 22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return(
         <div className={s.dialogs}>
                 <div className={s.dialogsItems}>
-                    <div className={s.dialog + ' ' + s.active}>Vladimir</div>
-                    <div className={s.dialog}>Ilon</div>
-                    <div className={s.dialog}>Sergey</div>
-                    <div className={s.dialog}>Elvira</div>
+                    <div className={s.dialog + ' ' + s.active}>
+                        <NavLink to='/messages/1'>Vladimir</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to='/messages/2'>Ilon</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to='/messages/3'>Sergey</NavLink>
+                    </div>
+                    <div className={s.dialog}>
+                        <NavLink to='/messages/4'>Elvira</NavLink>
+                    </div>
                 </div>
             <div className={s.messages}>
                 <div className={s.message}>Будет новый закон</div>
