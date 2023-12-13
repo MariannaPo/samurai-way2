7d111e8781ce96f7dc59abce483e00646ff31136
Marianna_Po <mapodmasko@gmail.com>
2023-12-13 19:49:21 +0300

    correct after 26

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,21 +1,10 @@
 import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+import {MessagesItems} from "./MessagesItems/MessageItems";
+import {Message} from "./Message/Message";
 
-const MessagesItems = (props: { id: string; name: string }) => {
-    let path = '/messages/' + props.id;
-    return(
-        <div className={s.dialog}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
 
-const Message = (props: {message: string}) => {
-    return(
-        <div className={s.message}>{props.message}</div>
-    )
-}
 export const Dialogs = () => {
 
     let dialogsData = [
